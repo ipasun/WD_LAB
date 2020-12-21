edeba4470b78726883f761cf1e2bb1d9c4790378
ipasun <ipayatsyshyn@gmail.com>
2020-12-21 09:57:47 +0200

    fix_number

--- LAB_2/app.js
+++ LAB_2/app.js
@@ -2,11 +2,11 @@ mod ();
 function mod() {
 	var num = Number(3);
 	var c;
-	c = num%6+1;
+	c = num%8+1;
 	document.write("<br/>");
 	document.write("<br/>");
 	document.write("Розв'язати приклад:"+" "+"num%6+1"+";"+"<br/>");
 	document.write("num ="+" "+num+";"+"<br/>");
-	document.write("3 % 6 = "+num % 6+";"+"<br/>");
+	document.write("3 % 8 = "+num % 8+";"+"<br/>");
 	document.write("3 + 1 = "+ c+"."+"<br/>");
 }
