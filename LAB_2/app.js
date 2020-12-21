829dde0ac8b0c2f018452087ae1e360718df410b
ipasun <ipayatsyshyn@gmail.com>
2020-12-21 10:02:12 +0200

    fix_number

--- LAB_2/app.js
+++ LAB_2/app.js
@@ -5,7 +5,7 @@ function mod() {
 	c = num%8+1;
 	document.write("<br/>");
 	document.write("<br/>");
-	document.write("Розв'язати приклад:"+" "+"num%6+1"+";"+"<br/>");
+	document.write("Розв'язати приклад:"+" "+"num%8+1"+";"+"<br/>");
 	document.write("num ="+" "+num+";"+"<br/>");
 	document.write("3 % 8 = "+num % 8+";"+"<br/>");
 	document.write("3 + 1 = "+ c+"."+"<br/>");
